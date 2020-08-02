a88b171875ca26bf222373e5f80f633320ec6d32
Hung Nguyen <hungnt89@gmail.com>
2020-08-02 21:21:22 +0700

    Use app_name='api'

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from api import views as api_views
 
+app_name = "api"
+
 urlpatterns = [
     path("books/", api_views.BookList.as_view(), name="book-list"),
     path("books/<int:pk>/", api_views.BookDetail.as_view(), name="book-detail"),
