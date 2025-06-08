ae2901486e378752d1ef156d208faed6d172aeba
Hung Nguyen <hungnt89@gmail.com>
2025-06-09 00:52:45 +0700

    Add slug, uuid for models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import date
+from django_extensions.db.fields import AutoSlugField
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
@@ -10,97 +11,77 @@ from locallibrary.models import MyBaseModel
 
 class Genre(MyBaseModel):
     """A Model representing Book's Genre"""
+
     name = models.CharField(
         max_length=200,
-        help_text=_('Enter a book genre. (e.g Science Fiction)')
+        help_text=_("Enter a book genre. (e.g Science Fiction)"),
+        unique=True,
     )
 
     class Meta:
-        ordering = ('name',)
+        ordering = ("name",)
 
     def __str__(self):
         """String representing a Genre"""
-        return self.name
+        return f"{self.name}"
 
 
 class Book(MyBaseModel):
     """A class representing a Book (not particular copy)."""
 
     title = models.CharField(
-        max_length=200,
-        help_text=_('Title of the book.'),
-        blank=False,
-        null=False
+        max_length=200, help_text=_("Title of the book."), blank=False, null=False
     )
+    slug = AutoSlugField(populate_from=["title"])
+
     author = models.ForeignKey(
-        'Author',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
+        "Author", on_delete=models.SET_NULL, blank=True, null=True
     )
     summary = models.TextField(
         max_length=1000,
-        help_text=_('Enter a brief description of the book.'),
+        help_text=_("Enter a brief description of the book."),
         blank=True,
-        null=True
+        null=True,
     )
     isbn = models.CharField(
-        _('ISBN'),
+        _("ISBN"),
         max_length=13,
         help_text='<a href="https://en.wikipedia.org/wiki/International_Standard_Book_Number">ISBN Number</a>',
         blank=True,
-        null=True
+        null=True,
     )
     genre = models.ManyToManyField(
         Genre,
-        help_text=_('Select genres for this book.'),
+        help_text=_("Select genres for this book."),
         blank=True,
-        null=True
     )
     language = models.ForeignKey(
-        'Language',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
+        "Language", on_delete=models.SET_NULL, blank=True, null=True
     )
 
     class Meta:
-        ordering = ('title',)
+        ordering = ("title",)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
     def get_absolute_url(self):
-        return reverse('book-detail', args=[str(self.pk)])
+        return reverse("book-detail", args=[str(self.pk)])
 
     def display_genre(self):
         """String showing genre of book"""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
-    display_genre.short_description = 'Genre'
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = "Genre"
 
 
 class BookInstance(MyBaseModel):
     """Model representing a particular copy of Book."""
 
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        help_text=_('Unique ID for a particular copy of book.')
-    )
-    book = models.ForeignKey(
-        'Book',
-        on_delete=models.SET_NULL,
-        null=True
-    )
-    imprint = models.CharField(
-        max_length=200,
-        blank=True,
-        default='Some Imprint X'
-    )
-    due_back = models.DateField(
-        blank=True,
-        null=True
-    )
+    book = models.ForeignKey("Book", on_delete=models.SET_NULL, null=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    imprint = models.CharField(max_length=200, blank=True, default="Some Imprint X")
+    due_back = models.DateField(blank=True, null=True)
     borrower = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
@@ -109,26 +90,26 @@ class BookInstance(MyBaseModel):
     )
 
     LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
+        ("m", "Maintenance"),
+        ("o", "On loan"),
+        ("a", "Available"),
+        ("r", "Reserved"),
     )
 
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
         blank=True,
-        default='m',
-        help_text=_('Book Availability'),
+        default="m",
+        help_text=_("Book Availability"),
     )
 
     class Meta:
-        ordering = ['due_back']
+        ordering = ["due_back"]
         permissions = (("can_mark_returned", "Set books as return"),)
 
     def __str__(self):
-        return f'{self.id} ({self.book.title})'
+        return f"{self.pk} ({self.book.title})"
 
     @property
     def is_overdue(self):
@@ -141,66 +122,57 @@ class BookInstance(MyBaseModel):
 
     @property
     def is_on_loan(self):
-        return (self.status == 'o' and
-                self.borrower is not None)
+        return self.status == "o" and self.borrower is not None
 
     @property
     def is_available(self):
-        return (self.status == 'a' and
-                self.borrower is None)
+        return self.status == "a" and self.borrower is None
 
 
 class Author(MyBaseModel):
     """Model represent an Author"""
-    first_name = models.CharField(
-        max_length=100
-    )
-    last_name = models.CharField(
-        max_length=100
-    )
-    date_of_birth = models.DateField(
-        _('Born'),
-        blank=True,
-        null=True
-    )
-    date_of_death = models.DateField(
-        _('Died'),
-        blank=True,
-        null=True
-    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(_("Born"), blank=True, null=True)
+    date_of_death = models.DateField(_("Died"), blank=True, null=True)
+    slug = AutoSlugField(populate_from=["first_name", "last_name"])
 
     class Meta:
-        ordering = ('last_name', 'first_name')
+        ordering = ("last_name", "first_name")
         permissions = (
-            ('can_crud_author', 'Can create, retrieve, update, delete author'),
+            ("can_crud_author", "Can create, retrieve, update, delete author"),
         )
 
     def get_absolute_url(self):
-        return reverse('author-detail', kwargs={'pk': self.id})
+        return reverse("author-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'
+        return f"{self.last_name}, {self.first_name}"
 
 
 class Language(MyBaseModel):
     """A model representing Languages in which Books are written."""
+
     LANGUAGES = (
-        ('en', 'English'),
-        ('fr', 'French'),
-        ('de', 'German'),
-        ('es', 'Spanish'),
-        ('fa', 'Farsi'),
-        ('vi', 'Vietnamese'),
-        ('kr', 'Korean'),
-        ('jp', 'Japanese'),
+        ("en", "English"),
+        ("fr", "French"),
+        ("de", "German"),
+        ("es", "Spanish"),
+        ("fa", "Farsi"),
+        ("vi", "Vietnamese"),
+        ("kr", "Korean"),
+        ("jp", "Japanese"),
     )
+
     name = models.CharField(
         max_length=2,
         choices=LANGUAGES,
-        default='en',
+        default="en",
         blank=True,
-        help_text=_('Select language for this book')
+        unique=True,
+        help_text=_("Select language for this book"),
     )
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
--- locallibrary/models.py
+++ locallibrary/models.py
@@ -1,22 +1,14 @@
 from django.db import models
-from secrets import token_hex
-
-
-def hex_token_20():
-    return token_hex(10)
 
 
 class MyBaseModel(models.Model):
-    id = models.CharField(
-        primary_key=True, max_length=20, blank=True, default=hex_token_20
-    )
-    time_created = models.DateTimeField(
-        auto_now_add=True, blank=True
-    )
-    time_updated = models.DateTimeField(
-        auto_now=True, blank=True
-    )
+    time_created = models.DateTimeField(auto_now_add=True, blank=True)
+    time_updated = models.DateTimeField(auto_now=True, blank=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         abstract = True
-        ordering = ('-time_created',)
+        ordering = ("-time_created",)
+
+
+# Nguyen Thanh Hung
