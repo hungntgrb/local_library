85c81b1cd632f8e5fa9e3b0c5f9ad68c4e951e5e
Hung Nguyen <hungnt89@gmail.com>
2025-06-10 16:11:48 +0700

    Update fake-data generators

--- mysnippets/fake_data.py
+++ mysnippets/fake_data.py
@@ -1,26 +1,35 @@
 from secrets import choice
 from faker import Faker
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission, Group
 
-from catalog.models import (Author, Book, Language, Genre, BookInstance)
+from catalog.models import Author, Book, Language, Genre, BookInstance
+from users.models import User
 
 fake = Faker()
 
+_PASSWORD = "AStrongPassword"
+
 
 def populate_languages():
-    languages = ('en', 'fr', 'de', 'es', 'jp', 'vi', 'kr', 'ca')
+    languages = ("en", "fr", "de", "es", "jp", "vi", "kr", "ca")
     for lang in languages:
-        Language.objects.create(
-            name=lang
-        )
+        Language.objects.create(name=lang)
 
 
 def populate_genres():
-    genres = ('Adventure', 'Horror', 'Romance', 'Travel',
-              'Academy', 'Professional', 'Manga', 'Comic')
+    genres = (
+        "Adventure",
+        "Horror",
+        "Romance",
+        "Travel",
+        "Academy",
+        "Professional",
+        "Manga",
+        "Comic",
+    )
     for genre in genres:
-        Genre.objects.create(
-            name=genre
-        )
+        Genre.objects.create(name=genre)
 
 
 def populate_authors(num=5):
@@ -29,7 +38,7 @@ def populate_authors(num=5):
             first_name=fake.first_name(),
             last_name=fake.last_name(),
             date_of_birth=fake.date_of_birth(),
-            date_of_death=fake.date_of_birth()
+            date_of_death=fake.date_of_birth(),
         )
 
 
@@ -38,8 +47,8 @@ def populate_books(num=5):
     languages = Language.objects.all()
 
     for i in range(num):
-        title = ' '.join(fake.words(6))
-        summary = ' '.join(fake.texts())
+        title = " ".join(fake.words(6))
+        summary = " ".join(fake.texts())
         Book.objects.create(
             title=title,
             summary=summary,
@@ -53,14 +62,8 @@ def populate_book_instances():
     books = Book.objects.all()
 
     for book in books:
-        BookInstance.objects.create(
-            book=book,
-            status='a'
-        )
-        BookInstance.objects.create(
-            book=book,
-            status='m'
-        )
+        BookInstance.objects.create(book=book, status="a")
+        BookInstance.objects.create(book=book, status="m")
 
 
 def update_book_genre():
@@ -83,7 +86,58 @@ def delete_book_instances():
     BookInstance.objects.all().delete()
 
 
+def add_some_users_and_groups():
+    group_thuthu = Group.objects.create(name="ThuThu")
+    users = []
+    password = _PASSWORD
+    users_data = [
+        {"username": "thuthu1", "email": "thuthu1@thuvien.com"},
+        {"username": "thuthu2", "email": "thuthu2@thuvien.com"},
+        {"username": "khach1", "email": "khach1@email.com"},
+        {"username": "khach2", "email": "khach2@email.com"},
+    ]
+    for data in users_data:
+        user = User.objects.create_user(
+            username=data["username"], email=data["email"], password=password
+        )
+
+        if data["username"].startswith("thuthu"):
+            group_thuthu.user_set.add(user)
+
+        users.append(user)
+    print(f"\nAdded {users}")
+
+
+# gnuH hnahT neyugN viet code nay
+
+
+def add_some_permissions():
+    book_content_type = ContentType.objects.get_for_model(BookInstance)
+    cho_muon_sach = Permission.objects.create(
+        codename="cho_muon_sach",
+        name="Can cho muon sach",
+        content_type=book_content_type,
+    )
+
+
 def main():
-    pass
+    populate_languages()
+    populate_genres()
+    populate_authors(num=6)
+    populate_books(num=8)
+    populate_book_instances()
+
+
+def main2():
+    update_book_genre()
+    add_some_users_and_groups()
+    add_some_permissions()
+
+
+def main3():
+    populate_authors(num=10)
+    populate_books(num=12)
+    populate_book_instances()
+
 
 # from mysnippets.fake_data import *
