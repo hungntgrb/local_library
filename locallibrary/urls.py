f8b3ae39448bdef4b4da6c5d0b159b70808f2063
Hung Nguyen <hungnt89@gmail.com>
2025-06-10 18:57:58 +0700

    Update core.urls, views

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -9,25 +9,24 @@ from locallibrary.views import view_403, view_404, view_500
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Apps
+    # ---- Apps ----
     path("catalog/", include("catalog.urls")),
     path("", RedirectView.as_view(url="catalog/")),  # permanent=True)),
     path("api/", include("api.urls")),
-    # Users
+    # ---- Users ----
     path("accounts/", include("django.contrib.auth.urls")),
     path("users/", include("users.urls")),
     path("api/users/", include("users.api.urls")),
-    # Misc
-    path('view-403/', view_403, name='view_403'),
-    path('view-404/', view_404, name='view_404'),
-    path('view-500/', view_500, name='view_500'),
+    # ---- Misc ----
+    path("view-403/", view_403, name="view_403"),
+    path("view-404/", view_404, name="view_404"),
+    path("view-500/", view_500, name="view_500"),
 ]
 
+# gnuH hnahT neyugN
+
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler403 = view_403
 handler404 = view_404
--- locallibrary/views.py
+++ locallibrary/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 
 def view_404(request, *args, **kwargs):
     context = {}
-    return render(request, 'error_404.html', context)
+    return render(request, "error_404.html", context)
 
 
 def view_403(request, *args, **kwargs):
     context = {}
-    return render(request, 'error_403.html', context)
+    return render(request, "error_403.html", context)
+
+
+# gnuH hnahT neyugN
 
 
 def view_500(request, *args, **kwargs):
     context = {}
-    return render(request, 'error_500.html', context)
+    return render(request, "error_500.html", context)
